c86d8ddd351427d7899a4f219431c6fb7bb3915a
Ascyt <filip.schauer07@gmail.com>
2023-11-18 16:46:18 +0100

    Updated example

--- exampleusage.py
+++ exampleusage.py
@@ -1,7 +1,16 @@
 import easygpt
 
-gpt = easygpt.gpt()
+prompt = 'give python factorial number'
 
-print(gpt.get(user="hello gpt :)"))
-print(gpt.get(messages=gpt.previous, user='what\'s up?.'))
-print(gpt.get(messages=gpt.previous, user='what was my first message?'))
+gpt = easygpt.gpt(logs=True)
+
+print('Improve prompt...')
+improved:str = gpt.get(user=prompt, system="You will be provided with a prompt. Your task is not to implement it, but to correct it and improve its phrasing.")
+
+print('Implement prompt...')
+implemented:str = gpt.get(user=improved + " Keep yourself short.", messages=[])
+print('Generate explanation...')
+explanation:str = gpt.get(messages=gpt.previous, user="Now explain this code in a simple and short way.")
+
+print(f'Implementation:\n\n{implemented}')
+print(f'Explanation:\n\n{explanation}')
